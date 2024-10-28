0bd8e03ec3e1695f34ec5f0468eda4369389a782
Batchuluun0605 <batchuluun@gmail.com>
2024-10-28 13:35:51 +0800

    create category

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,14 +1,125 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { TransactionType } from "../../../lib/types";
+import { useQuery } from "@tanstack/react-query";
+import { Category } from "@prisma/client";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import CreateCategoryDialog from "./CreateCategoryDialog";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface Props {
   type: TransactionType;
 }
 
+interface CategoriesQuery {
+  data?: Category[];
+}
+
 const CategoryPicker = ({ type }: Props) => {
-  return <div>CategoryPicker</div>;
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  const categoriesQuery = useQuery({
+    queryKey: ["categories", type],
+    queryFn: async () => {
+      const response = await fetch(`/api/categories?type=${type}`);
+
+      // Check if the response is okay (status 200-299)
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      return response.json(); // Return the parsed JSON
+    },
+  });
+
+  const selectedCategory = categoriesQuery.data?.find(
+    (category: Category) => category.name === value
+  );
+  console.log(categoriesQuery.data);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {selectedCategory ? (
+            <CategoryRow category={selectedCategory} />
+          ) : (
+            "Select category"
+          )}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <CommandInput placeholder="Search category..." />
+          <CreateCategoryDialog type={type} />
+
+          <CommandEmpty>
+            <p>Category not found</p>
+            <p className="text-xs text-muted-foreground">
+              Tip:Create a new category
+            </p>
+          </CommandEmpty>
+          <CommandGroup>
+            <CommandList>
+              {categoriesQuery.data &&
+                categoriesQuery.data.map((category: Category) => (
+                  <CommandItem
+                    key={category.name}
+                    onSelect={() => {
+                      setValue(category.name);
+                      setOpen((prev) => !prev);
+                    }}
+                  >
+                    <CategoryRow category={category} />
+                    <Check
+                      className={cn(
+                        "mr-2 w-4 h-4 opacity-0",
+                        value === category.name && "opacity-100"
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+            </CommandList>
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default CategoryPicker;
+
+function CategoryRow({ category }: { category: Category }) {
+  return (
+    <div className="flex items-center gap-2">
+      <span role="img">{category.icon}</span>
+      <span>{category.name}</span>
+    </div>
+  );
+}
